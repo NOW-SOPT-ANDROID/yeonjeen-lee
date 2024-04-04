a4a1ae8be517ba688e44c79da528d5e6c85d5428
yeonjeen <jeendev2019@gmail.com>
2024-04-04 20:37:40 +0900

    feat: 메인(마이페이지) 구현

--- app/src/main/java/com/sopt/now/Activity/MainActivity.kt
+++ app/src/main/java/com/sopt/now/Activity/MainActivity.kt
@@ -2,12 +2,27 @@ package com.sopt.now.Activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.sopt.now.R
+import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val id = intent.getStringExtra("id") ?: ""
+        val password = intent.getStringExtra("password") ?: ""
+        val nickname = intent.getStringExtra("nickname") ?: ""
+        val mbti = intent.getStringExtra("mbti") ?: ""
+
+        setMainProfile(id, password, nickname, mbti)
+    }
+
+    private fun setMainProfile(id: String, password: String, nickname: String, mbti: String) {
+        binding.tvMainId.text = id
+        binding.tvMainPs.text = password
+        binding.tvMainNickName.text = nickname
+        binding.tvMainMbti.text = mbti
     }
 }
