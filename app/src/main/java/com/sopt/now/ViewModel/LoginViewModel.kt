4cce8e13b37177bef5d3ab2403f4e7be7245e5c2
yeonjeen <jeendev2019@gmail.com>
2024-04-04 20:10:16 +0900

    feat: 로그인 성공 조건

--- app/src/main/java/com/sopt/now/ViewModel/LoginViewModel.kt
+++ app/src/main/java/com/sopt/now/ViewModel/LoginViewModel.kt
@@ -1,4 +1,12 @@
 package com.sopt.now.ViewModel
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    fun checkLoginCredentials(inputId: String, inputPassword: String, savedId: String?, savedPassword: String?): Boolean {
+        if (savedId != null && savedPassword != null) {
+            return inputId == savedId && inputPassword == savedPassword
+        }
+        return false
+    }
 }
