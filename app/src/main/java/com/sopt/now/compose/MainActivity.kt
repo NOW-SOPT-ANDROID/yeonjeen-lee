9bb657ef20f914334a81a3cda21ccde92f932205
yeonjeen <jeendev2019@gmail.com>
2024-04-07 23:37:48 +0900

    feat: 메인 구현

--- app/src/main/java/com/sopt/now/compose/MainActivity.kt
+++ app/src/main/java/com/sopt/now/compose/MainActivity.kt
@@ -3,25 +3,101 @@ package com.sopt.now.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val signUpId = intent.getStringExtra("ID") ?: ""
+        val signUpPassword = intent.getStringExtra("PASSWORD") ?: ""
+        val signUpNickname = intent.getStringExtra("NICKNAME") ?: ""
+        val signUpAddress = intent.getStringExtra("ADDRESS") ?: ""
         setContent {
             NOWSOPTAndroidTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MainProfileContent(signUpId, signUpPassword, signUpNickname, signUpAddress)
                 }
             }
         }
     }
 }
+
+@Composable
+fun MainProfileContent(
+    signUpId: String,
+    signUpPassword: String,
+    signUpNickname: String,
+    signUpAddress: String
+) {
+    Row(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .clip(shape = RoundedCornerShape(16.dp))
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_yeonjeen),
+                contentDescription = "Profile Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        Column(
+            modifier = Modifier.padding(start = 16.dp)
+        ) {
+            Text(
+                text = "이연진",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = "안녕 나는 연진이야",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = "ID: $signUpId",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = "Password: $signUpPassword",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = "Nickname: $signUpNickname",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = "Address: $signUpAddress",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+        }
+    }
+}
+
+
+
