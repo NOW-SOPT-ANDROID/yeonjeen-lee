bc6b77dec8cfc161d6dbe5d14589edb926425365
yeonjeen <jeendev2019@gmail.com>
2024-04-12 09:21:41 +0900

    style: 줄바꿈삭제

--- app/src/main/java/com/sopt/now/compose/MainActivity.kt
+++ app/src/main/java/com/sopt/now/compose/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -117,9 +116,6 @@ fun MainProfileScreen(
         )
     }
 }
-
-
-
 
 @Preview(showBackground = true)
 @Composable
