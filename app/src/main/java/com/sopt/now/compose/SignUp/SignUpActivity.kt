c608470f1905e0ab6fbf0b8336953f6a8d31c5cb
yeonjeen <jeendev2019@gmail.com>
2024-04-17 23:52:12 +0900

    style: 줄바꿈삭제

--- app/src/main/java/com/sopt/now/compose/SignUp/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/compose/SignUp/SignUpActivity.kt
@@ -33,7 +33,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sopt.now.compose.Login.LoginActivity
