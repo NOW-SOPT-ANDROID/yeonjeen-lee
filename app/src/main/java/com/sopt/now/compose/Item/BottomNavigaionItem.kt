629f3946f25fca43290a1b9a3b28017b6471d974
yeonjeen <jeendev2019@gmail.com>
2024-04-19 12:07:50 +0900

    feat: 바텀내비게이션아이템 구현

---
+++ app/src/main/java/com/sopt/now/compose/Item/BottomNavigaionItem.kt
@@ -0,0 +1,10 @@
+package com.sopt.now.compose.Item
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigationItem(
+    val icon: ImageVector,
+    val label: String,
+)
+
+
